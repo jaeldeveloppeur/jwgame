19a1c0308338e0bd855d4587b5957e9fc7153d9e
Jael Pati <jael.pati@gmail.com>
2020-04-04 22:03:42 +0200

    ajout pop up en js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const boutonIndice = document.getElementById('bouton1');
+const bouton1 = document.getElementById('bouton1');
+const bouton2 = document.getElementById('bouton2');
+const bouton3 = document.getElementById('bouton3');
 
 let listeIndices = [["Mon fils Eléazar m'a succédé comme prêtre en Israël - Nombres 20:28"],["J'ai critiqué Moïse pour avoir épousé une femme Koushite - Nombres 12:1,2"],["Mes fils, Nadab et Abihou, ont été tués par Jéhovah - Lévitique 10:1,2"],["Mon bâton a bourgeonné, ce qui signifie que Jéhovah m'a choisi - Nombres 17:8"],["Mon bâton est devenu un gros serpent - Exode 7:9-12"],["Le Nil s'est transformé en sang lorsque je l'ai touché par mon bâton - Exode 7:20"],["J'ai représenté Moïse comme porte-parole - Exode 4:10-17,30"],["Je suis le premier Grand Prêtre en Israël - Exode 28"],["Ma soeur s'appelle Miriam et mon frère Moïse - Exode 15:20"]];
 
@@ -7,7 +9,10 @@ var click = -1;
 function ajoutIndice (){
     click += 1; 
     if (click > 8){
-        console.log("perdu");
+        function openModal(){
+            document.getElementById("modal").style.top = "150px";
+        }
+        openModal();
     }
     else{
         const indice = document.createElement("li");
@@ -18,5 +23,9 @@ function ajoutIndice (){
         
 };
 
-boutonIndice.addEventListener('click', ajoutIndice);
+function closeModal(){
+    document.getElementById("modal").style.top = "-300px";
+}
 
+
+bouton1.addEventListener('click', ajoutIndice);
